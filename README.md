0283638ec9ceaddc221c19037d6ca5a29ce8b7d7
Rishabh Desai <desai.rishab@gmail.com>
2021-05-12 18:30:24 +0530

    CopyConstructor code added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # core-java-snippets
+
+### coreJava project
+
+* copyConstructor 
+ 
---
+++ coreJava/src/pages/CopyConstructor.java
@@ -0,0 +1,29 @@
+package pages;
+
+class Test {
+	int i, j;
+
+	public Test(int i, int j) { 
+		this.i = i;
+		this.j = j;
+	}
+
+	public Test(Test obj) { // copy constructor- call by reference
+		this.i = obj.i;
+		this.j = obj.j;
+	
+	}
+
+}
+
+public class CopyConstructor {
+
+	public static void main(String[] args) {
+		Test t = new Test(5,10);
+		System.out.println(t.i+" :  "+t.j);
+		
+		Test t1 = new Test(t);
+		System.out.println(t1.i+" :  "+t1.j);
+	}
+
+}
