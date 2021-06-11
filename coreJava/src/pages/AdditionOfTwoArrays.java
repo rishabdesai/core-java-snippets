e06b7c4fddfe67f5f7a89b5b3cf2fe57a85b277f
Rishabh Desai <desai.rishab@gmail.com>
2021-06-11 15:05:11 +0530

    addition of  all numbers in two separate array

---
+++ coreJava/src/pages/AdditionOfTwoArrays.java
@@ -0,0 +1,31 @@
+package pages;
+
+public class AdditionOfTwoArrays {
+	
+	public static void main(String[] args) {
+		
+		int[] arr1 = {1,1,1};
+		int[] arr2= {2,2,2};
+		
+		int sum=0;
+		int sum1 = 0;
+		int sum2 = 0;
+		
+		for (int i = 0; i < arr1.length; i++) {
+			sum1 = sum1+arr1[i];
+		}
+		System.out.println(sum1);
+		
+		for (int i = 0; i < arr2.length; i++) {
+			sum2 = sum2+arr2[i];
+		}
+		
+		System.out.println(sum2);
+		
+		sum=sum1+sum2;
+		System.out.println(sum);
+		
+		
+	}
+
+}
